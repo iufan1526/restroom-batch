71d9d2ede9c4c243aa96fe0afb9f3b59e30ed957
kim <iufan1526@gmail.com>
2024-07-19 11:20:44 +0900

    프로젝트 세팅

---
+++ src/main/java/com/restroom/track/batch/RestrommBatchApplication.java
@@ -0,0 +1,13 @@
+package com.restroom.track.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrommBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrommBatchApplication.class, args);
+	}
+
+}
